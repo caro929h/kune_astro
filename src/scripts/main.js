d32e98be9bfc499c6f6718e39b5f1d4dc80ae3a8
Alberte Faghtmann <alberte_jubii@hotmail.com>
2023-03-17 13:36:16 +0100

    line up js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 // https://xnxgitagsngxibomplmc.supabase.co
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhueGdpdGFnc25neGlib21wbG1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzkwNTMyNjcsImV4cCI6MTk5NDYyOTI2N30.AJfZquFwCqKVXMLFNHaxoi_zanrT7MVwiRwzsAfVyFU
+
+fetch("https://xnxgitagsngxibomplmc.supabase.co/rest/v1/KUNE?select=name,date", {
+  method: "GET",
+  headers: {
+    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhueGdpdGFnc25neGlib21wbG1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzkwNTMyNjcsImV4cCI6MTk5NDYyOTI2N30.AJfZquFwCqKVXMLFNHaxoi_zanrT7MVwiRwzsAfVyFU",
+  },
+})
+  .then((res) => res.json())
+  .then(showData);
+
+function showData(items) {
+  console.log(items);
+  //items.forEach()
+}
